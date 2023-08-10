7dba2a284da816a583a9c538407351bfd81b5aa9
parvezticon <parvej@ticonsys.com>
2023-08-10 10:43:28 +0600

    simple string cod

---
+++ Others_practice_code/task1.py
@@ -0,0 +1,16 @@
+my_str = "good morning"
+print(my_str)
+
+not_str = not my_str
+print(not_str)
+
+my_bool = True or False
+
+print(my_bool==not_str)
+
+
+
+# when my_string has some values and execute the not_str line then here not_str makes the my_str's value 
+# bool type and by using "not" it return the False type bool vaule.  
+# 
+# inside the print function the condition just check the both bool's are qual or not
